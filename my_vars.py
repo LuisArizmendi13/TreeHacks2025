4291791b8fe237a8dad45506a298e21bafeb2c4e
LuisArizmendi13 <larizmen@stanford.edu>
2025-02-16 11:30:23 +0000

    please

---
+++ my_vars.py
@@ -0,0 +1,2 @@
+ROBO = 'Mieg9053HaxM0M3XT0fZ'
+MY_KEY = "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" 
