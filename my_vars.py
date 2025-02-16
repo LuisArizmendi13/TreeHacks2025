407121f5b66369ba4c3a69103577a6b552871aea
LuisArizmendi13 <larizmen@stanford.edu>
2025-02-16 16:22:32 +0000

    done

--- my_vars.py
+++ my_vars.py
@@ -1,2 +1,2 @@
 ROBO = 'Mieg9053HaxM0M3XT0fZ'
-MY_KEY = "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" 
+COW = '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' 
