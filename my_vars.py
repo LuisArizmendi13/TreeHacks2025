df1765d1814c4a73680bb486201ae78c06836d8a
LuisArizmendi13 <larizmen@stanford.edu>
2025-02-16 08:25:15 -0800

    Update my_vars.py

--- my_vars.py
+++ my_vars.py
@@ -1,2 +1,1 @@
-ROBO = 'Mieg9053HaxM0M3XT0fZ'
-COW = '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' 
+ROBO = 
